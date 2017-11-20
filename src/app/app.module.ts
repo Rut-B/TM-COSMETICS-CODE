45711eec0cd4807ef584122ca01e8f8f1a300b29
Biton <rut.biton@intel.com>
2017-11-20 10:53:55 +0200

    firs prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { AboutComponent } from './about/about.component';
+import { StoreComponent } from './store/store.component';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    AboutComponent,
+    StoreComponent,
+    DetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
